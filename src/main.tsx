c9e8de2fb72a9df254d7afa1371d35aada59091e
ArthurWang <legendaryarthur@163.com>
2024-10-01 21:06:30 +0800

    登录注册

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+function BookManage() {
+  return <div>book</div>;
+}
+
+function Login(){
+  return <div>login</div>;
+}
+
+function Register(){
+  return <div>register</div>;
+}
+
+const routes = [
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/",
+    element: <BookManage/>,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(<RouterProvider router={router}/>);
